c3e482423079b3decaf5d710db4153ac3620c81d
Naveen Sai <38736123+naveen675@users.noreply.github.com>
2021-05-16 21:51:55 +0530

    generates a dictionary of n : n*n pair
    generates a dictionary of n : n*n pair

---
+++ zero to one codes/dict_gen.py
@@ -0,0 +1,34 @@
+#this program generates dictionary of size n with key as n and value as n*n
+
+
+# def dict_gen(n):
+#     dic = {}
+#     for i in range(1,n+1):
+#         dic[i]=i*i
+#     return dic
+
+###### Using Dictionar Comprehension #######
+
+def dict_gen(n):
+    
+    return {k:k*k for k in range(1,n+1)}
+
+
+
+
+
+###### Using Generator #######
+# def dict_gen(n):
+#     for i in range(1,n+1):
+#         yield i*i
+
+
+if __name__=='__main__':
+
+    #print(dict_gen(10000000))
+    n=10000000000000
+    count=1
+    for i in dict_gen(n):
+        print(str(count) +':'+str(i)+',',end='')
+        count=count+1
+
