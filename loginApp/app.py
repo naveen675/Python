26b47ce4b46efb266638f55ce4d927a1e9d68054
unknown <spnaveen675@gmail.com>
2022-04-05 01:25:47 +0530

    Modified Login App

--- loginApp/app.py
+++ loginApp/app.py
@@ -1,40 +1,68 @@
-from ast import Global
-from flask import Flask,jsonify,request
+from flask import Flask, jsonify, make_response, request
+from werkzeug.security import generate_password_hash,check_password_hash
+from functools import wraps
 import jwt
 import datetime
-from functools import wraps
+import sqlite3
 
 app = Flask(__name__)
 
-token = None
+
 
 app.config['SECRET_KEY'] = 'naveensainidamanuri'
 
-def token_required(f):
-    @wraps(f)
-    def decorator(*args,**kwargs):
+# def token_required(f):
+#     @wraps(f)
+#     def decorator(*args,**kwargs):
         
-        print(token)
-        if not token:
-            return jsonify({'message':'Token is missing!'})
+#         print(token)
+#         if not token:
+#             return jsonify({'message':'Token is missing!'})
         
-        return f(*args, **kwargs)
-    return decorator 
+#         return f(*args, **kwargs)
+#     return decorator 
 
-@app.route('/update')
-@token_required
-def update():
-    return jsonify({'message':'This is a valid token'})
+# @app.route('/update')
+# @token_required
+# def update():
+#     return jsonify({'message':'This is a valid token'})
 
 @app.route('/login/<username>,<password>')
+# @app.route('/login', methods=['GET']) 
 def login(username,password):
+ 
+    if password == '123456':
 
-    if (password == '123456'):
+        token = jwt.encode({'user':username,'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=45)},app.config['SECRET_KEY'])
 
-        token = jwt.encode({'user':username,'exp':datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
-        return token
-    else:
-        return "invalid"
+        return jsonify({'token':token})
+    
+    return make_response('could not verify please do signup')
+
+
+@app.route('/signup/<name>,<username>,<password>')
+def signup(name,username,password):
+
+    conn = sqlite3.connect("user.db")
+    print("Connected to Db")
+
+    try:
+        query = f''' 
+                INSERT INTO USERINFO VALUES ({name},{username},{password})
+        '''
+        conn.execute(query)
+        conn.commit()
+        return "query Excuted Success fully"
+
+    except :
+        return "Execution failed"
+
+
+
+
+
+
+    
 
 if __name__ == '__main__':
     app.run(debug = True)
--- loginApp/test.py
+++ loginApp/test.py
@@ -1,15 +1,23 @@
 import sqlite3
 from flask import Flask
 
+
 conn = sqlite3.connect("user.db")
 print("Connected to Db")
 
-create = conn.execute('''CREATE TABLE USERINFO 
-        (Name STRING,
-        UserID STRING,
-        Password STRING);
-''')
-query = conn.execute("INSERT INTO USERINFO VALUES ('naveen','naveen675','122345')")
+# create = conn.execute('''CREATE TABLE USERINFO 
+#         (Name STRING,
+#         UserID STRING,
+#         Password STRING);
+# ''')
+
+name = 'sai'
+username = 'sai'
+password = '123456'
+
+query = f'''
+  INSERT INTO USERINFO('Name','UserID','Password') VALUES ({name},{username},{password});'''
+conn.execute(query)
 rows = conn.execute('''
     SELECT * FROM USERINFO ''')
 
@@ -20,5 +28,28 @@ for row in rows:
     print("Name "+ row[0])
     print("User ID "+ row[1])
     print("Password "+ str(row[2]))
+
+
+# app = Flask(__name__)
+
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
+# db = SQLAlchemy(app)
+
+# class Users(db.Model):
+
+#    id = db.Column(db.Integer, primary_key=True)
+#    name = db.Column(db.String(50))
+#    username = db.column(db.String(60))
+#    password = db.Column(db.String(50))
+
+
+# db.create_all()
+
+# if __name__ =="__main__":
+
+#     app.run(debug=True)
+
+
 
 
